e37b59f33b6f7e58d213e055efb872abf6f125a8
Samuel Durán García <samuel_duran1@ucne.edu.do>
2024-02-12 18:49:34 -0400

    contexto

---
+++ DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap1_SamuelDuran.Models;
+
+namespace Parcial1_Ap1_SamuelDuran.DAL;
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+
+    }
+
+	public DbSet<Metas> Metas { get; set; }
+}
