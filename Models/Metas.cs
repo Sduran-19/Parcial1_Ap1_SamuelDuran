b92cbd13249be470db00de970e4e123f408852df
Samuel Durán García <samuel_duran1@ucne.edu.do>
2024-02-12 18:19:06 -0400

    models metas

---
+++ Models/Metas.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Parcial1_Ap1_SamuelDuran.Models;
+
+public class Metas
+{
+	[Key]
+	public int MetaId { get; set; }
+
+	[Required(ErrorMessage = "Error, debe ingresar una descripcion")]
+	public string? Descripcion { get; set; }
+
+	[Required(ErrorMessage = "Error, debe ingresar un monto ")]
+	public int? Monto { get; set; }
+
+}
